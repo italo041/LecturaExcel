05f61aae8a40fd0be69743cdd71f6abfb9403fa2
arturo <>
2019-10-31 22:34:03 -0500

    Actualizacion interfaz principal

--- src/Formulario/Principal.java
+++ src/Formulario/Principal.java
@@ -27,21 +27,93 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        btnVerLibro = new javax.swing.JMenuItem();
+        btnAgreLibro = new javax.swing.JMenuItem();
+        btnEnviLibro = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        btnSalir = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(0, 51, 153));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 550, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 296, Short.MAX_VALUE)
+        );
+
+        jMenu1.setText("Opciones");
+
+        btnVerLibro.setText("Ver Libro");
+        btnVerLibro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVerLibroActionPerformed(evt);
+            }
+        });
+        jMenu1.add(btnVerLibro);
+
+        btnAgreLibro.setText("Agregar Libro");
+        btnAgreLibro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgreLibroActionPerformed(evt);
+            }
+        });
+        jMenu1.add(btnAgreLibro);
+
+        btnEnviLibro.setText("Enviar Libro");
+        btnEnviLibro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEnviLibroActionPerformed(evt);
+            }
+        });
+        jMenu1.add(btnEnviLibro);
+        jMenu1.add(jSeparator1);
+
+        btnSalir.setText("Salir");
+        jMenu1.add(btnSalir);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Ayuda");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnVerLibroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVerLibroActionPerformed
+        new frmVerLibro(this, true).setVisible(true);
+    }//GEN-LAST:event_btnVerLibroActionPerformed
+
+    private void btnAgreLibroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgreLibroActionPerformed
+        new frmAgregrarLibro(this, true).setVisible(true);
+    }//GEN-LAST:event_btnAgreLibroActionPerformed
+
+    private void btnEnviLibroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEnviLibroActionPerformed
+        new frmMail(this, true).setVisible(true);
+    }//GEN-LAST:event_btnEnviLibroActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +151,14 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem btnAgreLibro;
+    private javax.swing.JMenuItem btnEnviLibro;
+    private javax.swing.JMenuItem btnSalir;
+    private javax.swing.JMenuItem btnVerLibro;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/Formulario/frmVerLibro.java
@@ -0,0 +1,227 @@
+package Formulario;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+ 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+/**
+ *
+ * @author USP
+ */
+public class frmVerLibro extends javax.swing.JDialog {
+
+    /**
+     * Creates new form frmVerLibro
+     */
+    public frmVerLibro(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        LeerLibro();
+    }
+    
+    public void LeerLibro(){
+        String nombreArchivo = "archivo.xlsx";
+        String rutaArchivo = "libro/" + nombreArchivo;
+
+//        try (FileInputStream file = new FileInputStream(new File(rutaArchivo))) {
+//                // leer archivo excel
+//                XSSFWorkbook worbook = new XSSFWorkbook(file);
+//                //obtener la hoja que se va leer
+//                XSSFSheet sheet = worbook.getSheetAt(0);
+//                //obtener todas las filas de la hoja excel
+//                Iterator<Row> rowIterator = sheet.iterator();
+//
+//                Row row;
+//                // se recorre cada fila hasta el final
+//                while (rowIterator.hasNext()) {
+//                        row = rowIterator.next();
+//                        //se obtiene las celdas por fila
+//                        Iterator<Cell> cellIterator = row.cellIterator();
+//                        Cell cell;
+//                        //se recorre cada celda
+//                        while (cellIterator.hasNext()) {
+//                                // se obtiene la celda en específico y se la imprime
+//                                cell = cellIterator.next();
+//                                System.out.print(cell.getStringCellValue()+" | ");
+//                        }
+//                        System.out.println();
+//                }
+//        } catch (Exception e) {
+//                e.getMessage();
+//        }
+        JTable jtable= new JTable();
+        DefaultTableModel tableModel = new DefaultTableModel();
+        try {
+            XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(new File(rutaArchivo)));
+            XSSFSheet sheet = wb.getSheetAt(0);//primeta hoja            
+            Row row;
+            Cell cell;
+
+            //obtiene cantidad total de columnas con contenido
+            int maxCol = 0;
+            for (int a = 0; a <= sheet.getLastRowNum(); a++) {
+                if(sheet.getRow(a)!=null){
+                    if (sheet.getRow(a).getLastCellNum() > maxCol) {
+                        maxCol = sheet.getRow(a).getLastCellNum();
+                    }    
+                }                
+            }
+            if (maxCol > 0) {
+                //Añade encabezado a la tabla
+                for (int i = 1; i <= maxCol; i++) {
+                    tableModel.addColumn("Col." + i);
+                }                
+                //recorre fila por fila
+                Iterator<Row> rowIterator = sheet.iterator();
+                while (rowIterator.hasNext()) {
+
+                    int index = 0;
+                    row = rowIterator.next();
+
+                    Object[] obj = new Object[row.getLastCellNum()];
+                    Iterator<Cell> cellIterator = row.cellIterator();
+
+                    while (cellIterator.hasNext()) {
+                        cell = cellIterator.next();
+                        //contenido para celdas vacias
+                        while (index < cell.getColumnIndex()) {
+                            obj[index] = "";
+                            index += 1;
+                        }
+                        
+                        //extrae contenido de archivo excel
+                        switch (cell.getCellType()) {
+                            case BOOLEAN:
+                                obj[index] = cell.getBooleanCellValue();
+                                break;
+                            case NUMERIC:
+                                obj[index] = cell.getNumericCellValue();
+                                break;
+                            case STRING:
+                                obj[index] = cell.getStringCellValue();
+                                break;
+                            case BLANK:
+                                obj[index] = " ";
+                                break;
+                            case FORMULA:
+                                obj[index] = cell.getCellFormula();
+                                break;                           
+                            default:
+                                obj[index] = "";
+                                break;
+                        }                        
+                        index += 1;
+                    }
+                    tableModel.addRow(obj);
+                }
+                jTable1.setModel(tableModel);
+            }else{
+                JOptionPane.showMessageDialog(null, "Nada que importar", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (IOException ex) {
+            System.err.println("" + ex.getMessage());
+        }
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jTable1.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_ALL_COLUMNS);
+        jScrollPane1.setViewportView(jTable1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(frmVerLibro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(frmVerLibro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(frmVerLibro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(frmVerLibro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the dialog */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                frmVerLibro dialog = new frmVerLibro(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    // End of variables declaration//GEN-END:variables
+}
